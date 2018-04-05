4b5a28352d4e3cd243f46cd5d11ce451fe422000
Anubhav Chaudhary <achaudhary997@yahoo.in>
2018-04-05 10:06:55 +0530

    Added layout for GUI

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
 public class Controller {
+    public Button queryButton;
+    public TableView queryResultTable;
+
+    public void showQuery() {
+        queryButton.setText("Works");
+    }
 }
